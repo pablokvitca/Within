60d79930678ce6278caa9879c67f7135822f64bd
pablokvitca <pablokvitca@gmail.com>
2015-06-24 00:13:47 -0300

    Smooth Camera Movement Finished
    Only bugs when you zoom in on a 2nd object while still zooming in on the
    1st. If you zoom in again on the same object, it does not get bugged.
    Possible fix 1: stop using moving, just use Global.camMoving;
    Possible fix 2: before modifying this.moving, get ALL GameObjects, then
    their component ZoomInOut.moving and set it to false. THEN modify
    this.moving;

--- Assets/Scripts/ZoomInOut.cs
+++ Assets/Scripts/ZoomInOut.cs
@@ -2,70 +2,82 @@
 using System.Collections;
 
 public class ZoomInOut : MonoBehaviour {
-
+	
 	public GameObject cameraHolder;
 	public GameObject cameraPrev;
 	public float transitionDuration;
-	bool moving = false;
+
+	public bool moving = false;
 
 	Vector3 startPoint;
 	Quaternion startTurn;
 	Quaternion turnLength;
 
-	// Use this for initialization
-	void Start() {
-	}
+	Global glo;
 
-	// Update is called once per frame
-	void Update() {
-		if (moving) {
-			MoveAndTurnCamera (Camera.main, cameraHolder);
-		}
+	private Transform destHolder;
+
+	public float smooth = 1.5f; // The relative speed at which the camera will catch up.
+	public float near = 0.25f;
+
+	void Start() {
+		glo = GameObject.Find("ScriptGlobal").GetComponent<Global>();
+		Global.camMoving = moving;
+		destHolder = cameraHolder.transform;
 	}
 
 	public void Zoom(GameObject dclicked)
 	{
-		if (!(Camera.main.transform.position == cameraHolder.transform.position)) {
-			Debug.Log (dclicked.ToString () + "was LEFT Double Clicked!");
-			moving = true;
+		if (moving) {
+			if (!glo.IsNear (Camera.main.transform, cameraHolder.transform, near)) {
+				Debug.Log (dclicked.ToString () + "was LEFT Double Clicked!");
+				Debug.Log ("Zooming in");
+				destHolder = cameraHolder.transform;
+				Debug.Log ("Zooming in finished");
+				if (ZoomFinished (Camera.main.transform, destHolder))
+					moving = false;
+				else
+					moving = true;
+			} else {
+				Debug.Log ("Zooming out");
+				destHolder = cameraPrev.transform;
+				Debug.Log ("Zooming out finished");
+				if (ZoomFinished (Camera.main.transform, destHolder))
+					moving = false;
+				else
+					moving = true;
+			}
 		} else {
-			Debug.Log ("Zooming out");
-			MoveAndTurnCamera(Camera.main, cameraPrev);
-			Debug.Log ("Zooming out finished");
+			Debug.Log("Camera is already moving. Please wait for it to finish.");
 		}
 	}
 
-	void MoveAndTurnCamera(Camera cam, GameObject holder) {
-		//Snaps
-		cam.transform.position = holder.transform.position;
-		cam.transform.rotation = holder.transform.rotation;
-		moving = false;
+	void Update() {
+		Global.camMoving = moving;
+		if (moving) {
+			// Lerp the camera's position between it's current position and it's new position.
+			SmoothTranslate(Camera.main, Camera.main.transform, destHolder, smooth);
+			SmoothLookAt(Camera.main, Camera.main.transform, destHolder, smooth);
+			if (ZoomFinished(Camera.main.transform, destHolder))
+				moving = false;
+			else
+				moving = true;
+
+			//Snaps
+			//cam.transform.position = holder.transform.position;
+			//cam.transform.rotation = holder.transform.rotation;
+		}
+	}
+	
+	void SmoothTranslate (Camera cam, Transform current, Transform dest, float timeFactor) {	
+		cam.transform.position = Vector3.Lerp(current.position, dest.position, timeFactor * Time.deltaTime);
+	}
+
+	void SmoothLookAt (Camera cam, Transform current, Transform dest, float timeFactor) {	
+		cam.transform.rotation = Quaternion.Lerp(current.rotation, dest.rotation, timeFactor * Time.deltaTime);
+	}
+
+	bool ZoomFinished (Transform current, Transform dest) {
+		return glo.IsNear (current, dest, near);
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
