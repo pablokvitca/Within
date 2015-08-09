cbae53ca38e660552b0e5965f8d2046125be0329
pablokvitca <pablokvitca@gmail.com>
2015-08-09 14:37:47 -0300

    Commit 09 08 2015 at 14 37
    Commit 09 08 2015 at 14 37

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	public void cST (string scene){
+			//Application.LoadLevel (scene);
+			Debug.Log(scene);
+
+	}
+}
---
+++ Assets/Scripts/Scrolling.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Scrolling : MonoBehaviour {
+
+	public GameObject flr;
+	public GUIText[] elementos;
+	public float creditsDelay = 5.0f;
+	public float creditsTime = 5.0f;
+	public float creditsSpeed = 0.2f;
+
+	void Start () {
+
+		//flr = GameObject.Find ("Filler");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		creditsDelay -= Time.deltaTime;
+
+		if (creditsDelay < 0) 
+		{
+			creditsTime -= Time.deltaTime;
+		}
+		if (creditsTime < 0) {
+
+			foreach (GUIText text in elementos)
+			{
+				if (flr.transform.position.y < 0.5f){
+
+				text.transform.Translate (Vector3.up * Time.deltaTime * creditsSpeed);
+
+				}
+			}
+		}
+
+	}
+}
